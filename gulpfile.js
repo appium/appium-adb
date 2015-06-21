e414cbc0222ab50beae2d23f964923f7caa78e20
moizjv <moizjv@gmail.com>
2015-06-21 16:01:06 -0700

    Adding e2e-test configurations

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 "use strict";
 
 var gulp = require('gulp'),
-    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp),
+    _ = require('lodash'),
+    DEFAULTS = require('appium-gulp-plugins').boilerplate.DEFAULTS,
+    argv = require('yargs').argv;
 
-boilerplate({build: 'appium-adb', jscs: false});
+boilerplate({
+  build: 'appium-adb',
+  jscs: false,
+  e2eTest: _.defaults({
+    android: argv.emu,
+    avd: argv.avd
+  }, DEFAULTS.e2eTest)
+});
