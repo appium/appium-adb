88afef78ae0209971b878ab9a36b8cabb2536d6d
Mykola Mokhnach <nick+1@wire.com>
2018-02-08 15:18:17 +0100

    Parse main activity from the manifest file using apkanalyzer tool (#296)
    * Parse main activity from the manifest file using apkanalyzer tool
    
    * Refactor method calls
    
    * Address refactoring issues
    
    * Add quotes

--- test/unit/android-manifest-specs.js
+++ test/unit/android-manifest-specs.js
@@ -2,6 +2,7 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import ADB from '../..';
 import * as teen_process from 'teen_process';
+import * as helpers from '../../lib/helpers.js';
 import { withMocks } from 'appium-test-support';
 
 
@@ -26,23 +27,213 @@ describe('android-manifest', function () {
       mocks.adb.verify();
     });
   }));
-  describe('packageAndLaunchActivityFromManifest', withMocks({adb, teen_process}, (mocks) => {
-    it('should correctly parse package and activity from manifest', async function () {
+  describe('packageAndLaunchActivityFromManifest', withMocks({adb, helpers, teen_process}, (mocks) => {
+    it('should correctly parse package and activity from manifest with apkanalyzer tool', async function () {
+      const apkanalyzerDummyPath = 'apkanalyzer';
+      mocks.helpers.expects("getApkanalyzerForOs").returns(apkanalyzerDummyPath);
+      const localApk = 'dummyAPK';
+      const dummyPackageName = 'io.appium.android';
+      const dummyActivityName = 'io.appium.mainactivity.MainTabActivity';
+      mocks.teen_process.expects("exec")
+        .once().withArgs(apkanalyzerDummyPath)
+        .returns({stdout: `
+        <?xml version="1.0" encoding="utf-8"?>
+        <manifest
+          xmlns:amazon="http://schemas.amazon.com/apk/res/android"
+          xmlns:android="http://schemas.android.com/apk/res/android"
+          android:versionCode="1234"
+          android:versionName="3.0.0"
+          android:installLocation="0"
+          package="${dummyPackageName}">
+
+          <application
+              android:theme="@ref/0x7f0f00ef"
+              android:label="@ref/0x7f0e00b4"
+              android:icon="@ref/0x7f0b0001"
+              android:name="io.appium.app.testappAppShell"
+              android:debuggable="false"
+              android:allowTaskReparenting="true"
+              android:allowBackup="false"
+              android:hardwareAccelerated="true"
+              android:supportsRtl="true">
+
+              <activity
+                  android:name="io.appium.mainactivity.MainTabActivity"
+                  android:exported="true"
+                  android:clearTaskOnLaunch="false"
+                  android:launchMode="1"
+                  android:screenOrientation="1"
+                  android:configChanges="0x4a0"
+                  android:alwaysRetainTaskState="true"
+                  android:windowSoftInputMode="0x30" />
+
+              <activity-alias
+                  android:name="${dummyActivityName}"
+                  android:exported="true"
+                  android:clearTaskOnLaunch="false"
+                  android:launchMode="1"
+                  android:screenOrientation="1"
+                  android:configChanges="0x4a0"
+                  android:targetActivity="io.appium.mainactivity.MainTabActivity"
+                  android:alwaysRetainTaskState="true"
+                  android:windowSoftInputMode="0x30">
+
+                  <intent-filter>
+
+                      <action
+                          android:name="android.intent.action.MAIN" />
+
+                      <category
+                          android:name="android.intent.category.LAUNCHER" />
+                  </intent-filter>
+
+                  <intent-filter>
+
+                      <action
+                          android:name="android.intent.action.VIEW" />
+
+                      <category
+                          android:name="android.intent.category.DEFAULT" />
+
+                      <category
+                          android:name="android.intent.category.BROWSABLE" />
+
+                      <data
+                          android:scheme="testapp"
+                          android:host="headline_event" />
+
+                      <data
+                          android:scheme="testapp"
+                          android:host="story-camera" />
+
+                      <data
+                          android:scheme="testapp"
+                          android:host="direct-inbox" />
+
+                      <data
+                          android:scheme="testapp"
+                          android:host="share" />
+                  </intent-filter>
+              </activity-alias>
+
+              <activity
+                  android:name="io.appium.mainactivity.MainActivity"
+                  android:exported="true"
+                  android:clearTaskOnLaunch="false"
+                  android:launchMode="1"
+                  android:screenOrientation="1"
+                  android:configChanges="0x4a0"
+                  android:alwaysRetainTaskState="true"
+                  android:windowSoftInputMode="0x30" />
+
+              <activity
+                  android:name="io.appium.nux.activity.SignedOutFragmentActivity"
+                  android:screenOrientation="1"
+                  android:configChanges="0x4a0"
+                  android:windowSoftInputMode="0x2" />
+
+              <activity
+                  android:name="io.appium.nux.impl.OnboardingActivity"
+                  android:exported="false"
+                  android:screenOrientation="1"
+                  android:configChanges="0x4a0"
+                  android:windowSoftInputMode="0x2" />
+
+              <activity
+                  android:theme="@ref/0x7f0f009e"
+                  android:name="io.appium.creation.activity.MediaCaptureActivity"
+                  android:screenOrientation="1" />
+
+              <activity
+                  android:theme="@ref/0x7f0f009e"
+                  android:name="io.appium.video.videocall.activity.VideoCallActivity"
+                  android:exported="false"
+                  android:launchMode="1"
+                  android:screenOrientation="1"
+                  android:configChanges="0x4a0"
+                  android:windowSoftInputMode="0x2" />
+
+              <activity
+                  android:name="io.appium.bugreporter.BugReporterActivity"
+                  android:launchMode="2"
+                  android:screenOrientation="1" />
+
+              <activity
+                  android:name="io.appium.osversionblock.OsVersionBlockingActivity"
+                  android:exported="false"
+                  android:screenOrientation="1" />
+
+              <activity
+                  android:name="io.appium.share.twitter.TwitterOAuthActivity"
+                  android:configChanges="0x4a0" />
+
+              <activity
+                  android:name="io.appium.share.tumblr.TumblrAuthActivity" />
+
+              <activity
+                  android:name="io.appium.share.vkontakte.VkontakteAuthActivity" />
+
+              <activity
+                  android:name="io.appium.share.ameba.AmebaAuthActivity" />
+
+              <activity
+                  android:name="io.appium.share.odnoklassniki.OdnoklassnikiAuthActivity" />
+
+              <activity
+                  android:name="io.appium.mainactivity.ActivityInTab"
+                  android:screenOrientation="1"
+                  android:configChanges="0x4a0" />
+
+              <activity
+                  android:name="io.appium.business.instantexperiences.ui.InstantExperiencesBrowserActivity"
+                  android:exported="false"
+                  android:launchMode="2"
+                  android:configChanges="0x5b0"
+                  android:windowSoftInputMode="0x10" />
+
+              <service
+                  android:name="io.appium.inappbrowser.service.BrowserLiteCallbackService"
+                  android:exported="false">
+
+                  <intent-filter>
+
+                      <action
+                          android:name="io.appium.browser.lite.BrowserLiteCallback" />
+                  </intent-filter>
+              </service>
+
+              <service
+                  android:name="io.appium.browser.lite.BrowserLiteIntentService"
+                  android:exported="false"
+                  android:process=":browser" />
+          </application>
+      </manifest>`});
+      let {apkPackage, apkActivity} = (await adb.packageAndLaunchActivityFromManifest(localApk));
+      apkPackage.should.equal(dummyPackageName);
+      apkActivity.should.equal(dummyActivityName);
+      mocks.teen_process.verify();
+      mocks.helpers.verify();
+    });
+
+    it('should correctly parse package and activity from manifest with Appium Apk Tools fallback', async function () {
       adb.binaries.aapt = 'dummy_aapt';
-      const localApk = 'dummyAPK',
-            dummyPackageName = 'package',
-            dummyActivityName = 'activity';
+      const localApk = 'dummyAPK';
+      const dummyPackageName = 'package';
+      const dummyActivityName = 'activity';
+      mocks.helpers.expects("getApkanalyzerForOs").throws();
       mocks.adb.expects("initAapt")
         .once().withExactArgs()
-              .returns('');
+        .returns('');
       mocks.teen_process.expects("exec")
         .once().withExactArgs('dummy_aapt', ['dump', 'badging', localApk])
         .returns({stdout: ` package: name='${dummyPackageName}'
-                            launchable-activity: name='${dummyActivityName}'`});
+                          launchable-activity: name='${dummyActivityName}'`});
       let {apkPackage, apkActivity} = (await adb.packageAndLaunchActivityFromManifest(localApk));
       apkPackage.should.equal(dummyPackageName);
       apkActivity.should.equal(dummyActivityName);
       mocks.adb.verify();
+      mocks.teen_process.verify();
+      mocks.helpers.verify();
     });
   }));
   describe('hasInternetPermissionFromManifest', withMocks({adb, teen_process}, (mocks) => {
