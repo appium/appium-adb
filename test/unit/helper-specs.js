143d10f1846b6730a16217dd347f1fb89fda509f
Mykola Mokhnach <mokhnach@gmail.com>
2019-08-06 08:25:41 +0200

    Use aapt2 as a fallback for string resources extraction (#467)

--- test/unit/helper-specs.js
+++ test/unit/helper-specs.js
@@ -1,5 +1,8 @@
-import { getAndroidPlatformAndPath,
-         buildStartCmd, isShowingLockscreen, getBuildToolsDirs, parseManifest } from '../../lib/helpers';
+import {
+  getAndroidPlatformAndPath,
+  buildStartCmd, isShowingLockscreen, getBuildToolsDirs,
+  parseManifest, parseAaptStrings, parseAapt2Strings,
+} from '../../lib/helpers';
 import { withMocks } from 'appium-test-support';
 import { fs } from 'appium-support';
 import path from 'path';
@@ -320,6 +323,176 @@ describe('helpers', withMocks({fs}, function (mocks) {
       activity.should.eql('com.example.LaunchActivity');
       versionCode.should.eql(697);
       versionName.should.eql('3.27.697');
+    });
+  });
+
+  describe('parseAaptStrings', function () {
+    it('should parse strings received from aapt output', function () {
+      const aaptOutput = `
+      Package Groups (1)
+      Package Group 0 id=0x7f packageCount=1 name=io.appium.android.apis
+      Package 0 id=0x7f name=io.appium.android.apis
+        type 0 configCount=1 entryCount=6
+          config (default):
+            resource 0x7f0c0215 io.appium.android.apis:string/linear_layout_8_vertical: t=0x03 d=0x0000044c (s=0x0008 r=0x00)
+              (string16) "Vertical"
+            resource 0x7f0c0216 io.appium.android.apis:string/linear_layout_8_horizontal: t=0x03 d=0x0000044d (s=0x0008 r=0x00)
+              (string16) "Horizontal"
+          config fr:
+            resource 0x7f0c0215 io.appium.android.apis:string/linear_layout_8_vertical: t=0x03 d=0x0000044c (s=0x0008 r=0x00)
+              (string16) "Vertical"
+            resource 0x7f0c0216 io.appium.android.apis:string/linear_layout_8_horizontal: t=0x03 d=0x0000044d (s=0x0008 r=0x00)
+              (string16) "Horizontal"
+      `;
+      const aaptStrings = parseAaptStrings(aaptOutput, '(default)');
+      aaptStrings.linear_layout_8_horizontal.should.eql('Horizontal');
+    });
+    it('should parse plurals received from aapt output', function () {
+      const aaptOutput = `
+        Package Groups (1)
+        Package Group 0 id=0x7f packageCount=1 name=io.appium.test
+          Package 0 id=0x7f name=io.appium.test
+            type 0 configCount=1 entryCount=685
+              spec resource 0x7f010000 io.appium.test:attr/audioMessageDuration: flags=0x00000000
+              spec resource 0x7f010001 io.appium.test:attr/callingChatheadFooter: flags=0x00000000
+              spec resource 0x7f010002 io.appium.test:attr/callingChatheadInitials: flags=0x00000000
+              spec resource 0x7f010003 io.appium.test:attr/callingControlButtonLabel: flags=0x00000000
+              spec resource 0x7f010004 io.appium.test:attr/circleRadius: flags=0x00000000
+              config de-rDE:
+                resource 0x7f010000 io.appium.test:attr/audioMessageDuration: <bag>
+                  Parent=0x00000000(Resolved=0x7f000000), Count=1
+                  #0 (Key=0x01000000): (color) #00000001
+                resource 0x7f010001 io.appium.test:attr/callingChatheadFooter: <bag>
+                  Parent=0x00000000(Resolved=0x7f000000), Count=1
+                  #0 (Key=0x01000000): (color) #00000001
+              config de-rDE:
+                resource 0x7f080000 io.appium.test:string/abc_action_bar_home_description: t=0x03 d=0x00000c27 (s=0x0008 r=0x00)
+                  (string8) "Navigate \\"home\\""
+                resource 0x7f080001 io.appium.test:string/abc_action_bar_home_description_format: t=0x03 d=0x00000ad1 (s=0x0008 r=0x00)
+                  (string8) "%1$s, %2$s"
+                resource 0x7f080002 io.appium.test:string/abc_action_bar_home_subtitle_description_format: t=0x03 d=0x00000ad0 (s=0x0008 r=0x00)
+                  (string8) "%1$s, %2$s, %3$s"
+            type 1 configCount=1 entryCount=685
+              config de-rDE:
+                resource 0x7f0a0000 io.appium.test:plurals/calling__conversation_full__message: <bag>
+                  Parent=0x00000000(Resolved=0x7f000000), Count=2
+                  #0 (Key=0x01000004): (string8) "Calls work in conversations with up to 1 person."
+                  #1 (Key=0x01000005): (string8) "Calls work in conversations with up to %1$d people. \\"blabla\\""
+                resource 0x7f0a0001 io.appium.test:plurals/calling__voice_channel_full__message: <bag>
+                  Parent=0x00000000(Resolved=0x7f000000), Count=6
+                  #0 (Key=0x01000004): (string8) "There's only room for %1$d people in here."
+                  #1 (Key=0x01000005): (string8) "There's only room for %1$d people in here."
+                  #2 (Key=0x01000006): (string8) "There's only room for %1$d people in here."
+                  #3 (Key=0x01000007): (string8) "There's only room for %1$d people in here."
+                  #4 (Key=0x01000008): (string8) "There's only room for %1$d people in here."
+                  #5 (Key=0x01000009): (string8) "There's only room for %1$d people in here."
+            type 16 configCount=1 entryCount=8
+              spec resource 0x7f110000 io.appium.test:menu/conversation_header_menu_audio: flags=0x00000000
+              spec resource 0x7f110001 io.appium.test:menu/conversation_header_menu_collection: flags=0x00000000
+              spec resource 0x7f110002 io.appium.test:menu/conversation_header_menu_collection_searching: flags=0x00000000
+              spec resource 0x7f110003 io.appium.test:menu/conversation_header_menu_video: flags=0x00000000
+              spec resource 0x7f110004 io.appium.test:menu/conversation_multiuse: flags=0x00000000
+              spec resource 0x7f110005 io.appium.test:menu/toolbar_close_white: flags=0x00000000
+              spec resource 0x7f110006 io.appium.test:menu/toolbar_collection: flags=0x00000000
+              spec resource 0x7f110007 io.appium.test:menu/toolbar_sketch: flags=0x00000000
+              config (default):
+                resource 0x7f110000 io.appium.test:menu/conversation_header_menu_audio: t=0x03 d=0x000000b6 (s=0x0008 r=0x00)
+                  (string8) "res/menu/conversation_header_menu_audio.xml"
+                resource 0x7f110001 io.appium.test:menu/conversation_header_menu_collection: t=0x03 d=0x000000b7 (s=0x0008 r=0x00)
+                  (string8) "res/menu/conversation_header_menu_collection.xml"
+                resource 0x7f110002 io.appium.test:menu/conversation_header_menu_collection_searching: t=0x03 d=0x000000b8 (s=0x0008 r=0x00)
+                  (string8) "res/menu/conversation_header_menu_collection_searching.xml"
+                resource 0x7f110003 io.appium.test:menu/conversation_header_menu_video: t=0x03 d=0x000000b9 (s=0x0008 r=0x00)
+                  (string8) "res/menu/conversation_header_menu_video.xml"
+                resource 0x7f110004 io.appium.test:menu/conversation_multiuse: t=0x03 d=0x000000ba (s=0x0008 r=0x00)
+                  (string8) "res/menu/conversation_multiuse.xml"
+                resource 0x7f110005 io.appium.test:menu/toolbar_close_white: t=0x03 d=0x000000bb (s=0x0008 r=0x00)
+                  (string8) "res/menu/toolbar_close_white.xml"
+                resource 0x7f110006 io.appium.test:menu/toolbar_collection: t=0x03 d=0x000000bc (s=0x0008 r=0x00)
+                  (string8) "res/menu/toolbar_collection.xml"
+                resource 0x7f110007 io.appium.test:menu/toolbar_sketch: t=0x03 d=0x0000007f (s=0x0008 r=0x00)
+                  (string8) "res/menu/toolbar_sketch.xml"
+      `;
+      const aaptStrings = parseAaptStrings(aaptOutput, 'de-rDE');
+      aaptStrings.abc_action_bar_home_description.should.eql('Navigate "home"');
+      aaptStrings.calling__conversation_full__message.should.eql([
+        'Calls work in conversations with up to 1 person.',
+        'Calls work in conversations with up to %1$d people. "blabla"',
+      ]);
+    });
+  });
+
+  describe('parseAapt2Strings', function () {
+    it('should parse strings received from aapt2 output', function () {
+      const aapt2Output = `
+      Binary APK
+      Package name=io.appium.android.apis id=7f
+        type string id=0c entryCount=944
+          resource 0x7f0c0215 string/linear_layout_8_vertical
+            () "Vertical"
+            (fr) "Vertical"
+          resource 0x7f0c0216 string/linear_layout_8_horizontal
+            () "Horizontal"
+            (fr) "Horizontal"
+          resource 0x7f0c0217 string/linear_layout_8_top
+            () "Top"
+            (fr) "Top"
+      `;
+      const aapt2Strings = parseAapt2Strings(aapt2Output, '');
+      aapt2Strings.linear_layout_8_horizontal.should.eql('Horizontal');
+    });
+    it('should parse plurals received from aapt2 output', function () {
+      const aapt2Output = `
+      Binary APK
+      Package name=io.appium.android.apis id=7f
+        type plurals id=0d entryCount=27
+          resource 0x7f0d0002 plurals/connect_inbox__link__name
+            () (plurals) size=2
+              one="1 person waiting"
+              other="%1$s people waiting"
+            (da) (plurals) size=2
+              one="1 person venter"
+              other="%1$s personer venter"
+            (fa) (plurals) size=2
+              one="1 نفر در انتظار است"
+              other="%1$s در انتظار هستند"
+            (ja) (plurals) size=1
+              other="%1$s 人が待機中"
+            (de) (plurals) size=2
+              one="Eine Kontaktanfrage"
+              other="%1$s Kontaktanfragen
+
+              "blabla""
+            (fi) (plurals) size=2
+              one="1 ihminen odottaa"
+              other="%1$s ihmistä odottaa"
+            (sk) (plurals) size=4
+              one="1 osoba čaká"
+              few="%1$s ľudí čaká"
+              many="%1$s ľudí čaká"
+              other="%1$s ľudí čaká"
+            (uk) (plurals) size=4
+              one="1 людина очікує"
+              few="%1$s людей очікує"
+              many="%1$s людей очікує"
+              other="%1$s людей очікує"
+            (el) (plurals) size=2
+              one="1 άτομο σε αναμονή"
+              other="%1$s άτομα σε αναμονή"
+            (nl) (plurals) size=2
+              one="1 persoon wacht"
+              other="%1$s mensen die wachten"
+            (pl) (plurals) size=4
+              one="1 osoba czeka"
+              few="%1$s ludzi oczekujących"
+              many="%1$s ludzi oczekujących"
+              other="%1$s ludzi oczekujących"
+      `;
+      const aapt2Strings = parseAapt2Strings(aapt2Output, 'de');
+      aapt2Strings.connect_inbox__link__name.should.eql([
+        'Eine Kontaktanfrage',
+        '%1$s Kontaktanfragen\\n\\n"blabla"',
+      ]);
     });
   });
 }));
