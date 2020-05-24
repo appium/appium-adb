e922544c5c5c2a4238b9ec01e7ee438045ed2ce6
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-05-24 20:55:18 +0900

    add getAndroidBinaryPath

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import * as adb from './lib/adb';
 
 
-const { ADB, DEFAULT_ADB_PORT } = adb;
+const { ADB, DEFAULT_ADB_PORT, getAndroidBinaryPath } = adb;
 
 export default ADB;
-export { DEFAULT_ADB_PORT, ADB };
+export { DEFAULT_ADB_PORT, ADB, getAndroidBinaryPath };
