980afb5ce4880b29ea3ecc41681041abf83a1cf6
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-05-25 20:24:54 +0900

    feat: expose getSdkRootFromEnv (#534)

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 import * as adb from './lib/adb';
 
-
-const { ADB, DEFAULT_ADB_PORT, getAndroidBinaryPath } = adb;
+const {
+  ADB,
+  DEFAULT_ADB_PORT,
+  getAndroidBinaryPath,
+  getSdkRootFromEnv
+} = adb;
 
 export default ADB;
-export { DEFAULT_ADB_PORT, ADB, getAndroidBinaryPath };
+export { ADB, DEFAULT_ADB_PORT, getAndroidBinaryPath, getSdkRootFromEnv };
