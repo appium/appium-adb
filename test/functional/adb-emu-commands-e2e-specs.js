6cc5de99b90193194b0266648c187f1809b40f8b
Mykola Mokhnach <mokhnach@gmail.com>
2020-08-07 13:48:08 +0200

    Reenable tests

--- test/functional/adb-emu-commands-e2e-specs.js
+++ test/functional/adb-emu-commands-e2e-specs.js
@@ -1,46 +1,14 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import ADB from '../..';
-import path from 'path';
-import { rootDir } from '../../lib/helpers.js';
-import { sleep } from 'asyncbox';
-
 
 chai.use(chaiAsPromised);
 chai.should();
-
-const fingerprintPath = path.resolve(rootDir, 'test', 'fixtures', 'Fingerprint.apk');
-const pkg = 'com.example.fingerprint';
-const activity = '.MainActivity';
-const secretActivity = '.SecretActivity';
 
 describe('adb emu commands', function () {
   let adb;
   before(async function () {
     adb = await ADB.createADB();
-
-    // the test here only works if we have API level 23 or above
-    // it will also fail on emulators
-    if (await adb.getApiLevel() < 23 || !process.env.REAL_DEVICE) {
-      return this.skip();
-    }
-  });
-  it('fingerprint should open the secret activity on emitted valid finger touch event', async function () {
-    if (await adb.isAppInstalled(pkg)) {
-      await adb.forceStop(pkg);
-      await adb.uninstallApk(pkg);
-    }
-    await adb.install(fingerprintPath);
-    await adb.startApp({pkg, activity});
-    await sleep(500);
-
-    let app = await adb.getFocusedPackageAndActivity();
-    app.appActivity.should.equal(activity);
-    await adb.fingerprint(1111);
-    await sleep(2500);
-
-    app = await adb.getFocusedPackageAndActivity();
-    app.appActivity.should.equal(secretActivity);
   });
 
   describe('execEmuConsoleCommand', function () {
