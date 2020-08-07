092c45cac9de3f951229becf5f7056920b911e02
Mykola Mokhnach <mokhnach@gmail.com>
2020-08-07 20:50:25 +0200

    chore: Only use -delay-adb emulator startup detection if image API version is above 27 (#547)

--- test/functional/adb-emu-commands-e2e-specs.js
+++ test/functional/adb-emu-commands-e2e-specs.js
@@ -1,46 +1,20 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import ADB from '../..';
-import path from 'path';
-import { rootDir } from '../../lib/helpers.js';
-import { sleep } from 'asyncbox';
-
 
 chai.use(chaiAsPromised);
 chai.should();
 
-const fingerprintPath = path.resolve(rootDir, 'test', 'fixtures', 'Fingerprint.apk');
-const pkg = 'com.example.fingerprint';
-const activity = '.MainActivity';
-const secretActivity = '.SecretActivity';
-
 describe('adb emu commands', function () {
   let adb;
   before(async function () {
-    adb = await ADB.createADB();
-
-    // the test here only works if we have API level 23 or above
-    // it will also fail on emulators
-    if (await adb.getApiLevel() < 23 || !process.env.REAL_DEVICE) {
+    if (process.env.REAL_DEVICE) {
       return this.skip();
     }
-  });
-  it('fingerprint should open the secret activity on emitted valid finger touch event', async function () {
-    if (await adb.isAppInstalled(pkg)) {
-      await adb.forceStop(pkg);
-      await adb.uninstallApk(pkg);
-    }
-    await adb.install(fingerprintPath);
-    await adb.startApp({pkg, activity});
-    await sleep(500);
 
-    let app = await adb.getFocusedPackageAndActivity();
-    app.appActivity.should.equal(activity);
-    await adb.fingerprint(1111);
-    await sleep(2500);
-
-    app = await adb.getFocusedPackageAndActivity();
-    app.appActivity.should.equal(secretActivity);
+    adb = await ADB.createADB();
+    const devices = await adb.getConnectedEmulators();
+    adb.setDevice(devices[0]);
   });
 
   describe('execEmuConsoleCommand', function () {
@@ -60,6 +34,15 @@ describe('adb emu commands', function () {
       const {revision, buildId} = await adb.getEmuVersionInfo();
       revision.should.not.be.empty;
       (buildId > 0).should.be.true;
+    });
+  });
+
+  describe('getEmuImageProperties', function () {
+    it('should get emulator image properties', async function () {
+      const name = await adb.execEmuConsoleCommand(['avd', 'name']);
+      const {target} = await adb.getEmuImageProperties(name);
+      const apiMatch = /\d+/.exec(target);
+      (parseInt(apiMatch[0], 10) > 0).should.be.true;
     });
   });
 });
