6153bb547a473a477dc10a27affa8acc2cc51c97
Mykola Mokhnach <mokhnach@gmail.com>
2020-10-22 06:40:57 +0200

    chore: Perform apks cache cleanup on process exit (#556)

--- test/functional/helpers-specs-e2e-specs.js
+++ test/functional/helpers-specs-e2e-specs.js
@@ -5,19 +5,6 @@ import { getAndroidPlatformAndPath, requireSdkRoot } from '../../lib/helpers.js'
 chai.use(chaiAsPromised);
 
 describe('Helpers', function () {
-  it('requireSdkRoot should throw when no ANDROID_HOME is set', async function () {
-    let android_home = process.env.ANDROID_HOME;
-    // temp setting android_home to null.
-    delete process.env.ANDROID_HOME;
-
-    try {
-      await requireSdkRoot().should.eventually.be.rejectedWith(/environment/);
-    } finally {
-      // resetting ANDROID_HOME
-      process.env.ANDROID_HOME = android_home;
-    }
-  });
-
   it('getAndroidPlatformAndPath should return platform and path for android', async function () {
     const sdkRoot = await requireSdkRoot();
     const {platform, platformPath} = await getAndroidPlatformAndPath(sdkRoot);
