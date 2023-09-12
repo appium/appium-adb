3095bf991a871a8de1275f305a3cbbb52483fef3
Mykola Mokhnach <mokhnach@gmail.com>
2023-09-12 08:45:56 +0200

    fix: JSON data parsing with extras

--- test/unit/helper-specs.js
+++ test/unit/helper-specs.js
@@ -438,6 +438,15 @@ describe('helpers', withMocks({fs}, function (mocks) {
       items.length.should.eql(2);
       total.should.eql(2);
     });
+    it('should parse JSON received from broadcast output having extras', function () {
+      const broadcastOutput = `
+      Broadcasting: Intent { act=io.appium.settings.sms.read flg=0x400000 (has extras) }
+      Broadcast completed: result=-1, data="{"items":[{"id":"2","address":"+123456789","date":"1581936422203","read":"0","status":"-1","type":"1","body":"\\"text message2\\""},{"id":"1","address":"+123456789","date":"1581936382740","read":"0","status":"-1","type":"1","body":"\\"text message\\""}],"total":2}", extras: Bundle[mParcelledData.dataSize=52]
+      `;
+      const {items, total} = parseJsonData(broadcastOutput, '');
+      items.length.should.eql(2);
+      total.should.eql(2);
+    });
     it('should throw an error if json retrieval fails', function () {
       const broadcastOutput = `
       Broadcasting: Intent { act=io.appium.settings.sms.read flg=0x400000 (has extras) }
