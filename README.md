dcfe5a69eae2697629d2b9b8e92382a0081af70a
Mykola Mokhnach <mokhnach@gmail.com>
2024-01-12 22:53:05 +0100

    refactor!: Drop APIs related to io.appium.settings interactions (#716)

--- README.md
+++ README.md
@@ -4,9 +4,8 @@ appium-adb
 [![NPM version](http://img.shields.io/npm/v/appium-adb.svg)](https://npmjs.org/package/appium-adb)
 [![Downloads](http://img.shields.io/npm/dm/appium-adb.svg)](https://npmjs.org/package/appium-adb)
 
-A wrapper over android-adb, implemented using ES6 and along with `async/await`. This package is mainly used by Appium to perform all adb operations on android device.
-
-*Note*: Issue tracking for this repo has been disabled. Please use the [main Appium issue tracker](https://github.com/appium/appium/issues) instead.
+A wrapper over [Android Debugger Bridge](https://developer.android.com/tools/adb), implemented using ES6
+and along with `async/await`. This package is mainly used by Appium to perform all adb operations on Android devices.
 
 ## Installing
 
@@ -49,110 +48,3 @@ import ADB from 'appium-adb';
 const adb = await ADB.createADB();
 console.log(await adb.getPIDsByName('com.android.phone'));
 ```
-
-### List of methods:
-
-- `createADB`
-- `getAdbWithCorrectAdbPath`
-- `initAapt`
-- `initZipAlign`
-- `getApiLevel`
-- `isDeviceConnected`
-- `mkdir`
-- `isValidClass`
-- `forceStop`
-- `clear`
-- `stopAndClear`
-- `availableIMEs`
-- `enabledIMEs`
-- `enableIME`
-- `disableIME`
-- `setIME`
-- `defaultIME`
-- `keyevent`
-- `lock`
-- `back`
-- `goToHome`
-- `isScreenLocked`
-- `isSoftKeyboardPresent`
-- `sendTelnetCommand`
-- `isAirplaneModeOn`
-- `setAirplaneMode`
-- `broadcastAirplaneMode`
-- `isWifiOn`
-- `getScreenSize`
-- `getScreenDensity`
-- `setWifiState`
-- `isDataOn`
-- `setDataState`
-- `setWifiAndData`
-- `rimraf`
-- `push`
-- `pull`
-- `processExists`
-- `forwardPort`
-- `reversePort` (ApiLevel >=21)
-- `forwardAbstractPort`
-- `ping`
-- `restart`
-- `startLogcat`
-- `stopLogcat`
-- `getLogcatLogs`
-- `getPIDsByName`
-- `killProcessesByName`
-- `killProcessByPID`
-- `broadcastProcessEnd`
-- `broadcast`
-- `packageAndLaunchActivityFromManifest`
-- `compileManifest`
-- `insertManifest`
-- `hasInternetPermissionFromManifest`
-- `getSdkBinaryPath`
-- `getBinaryFromSdkRoot`
-- `getBinaryFromPath`
-- `getConnectedDevices`
-- `getDevicesWithRetry`
-- `restartAdb`
-- `adbExec`
-- `shell`
-- `getAdbServerPort`
-- `getEmulatorPort`
-- `getPortFromEmulatorString`
-- `getConnectedEmulators`
-- `setEmulatorPort`
-- `setDeviceId`
-- `getRunningAVD`
-- `getRunningAVDWithRetry`
-- `killAllEmulators`
-- `launchAVD`
-- `waitForEmulatorReady`
-- `waitForDevice`
-- `reboot`
-- `signWithDefaultCert`
-- `signWithCustomCert`
-- `sign`
-- `zipAlignApk`
-- `checkApkCert`
-- `checkCustomApkCert`
-- `getKeystoreHash`
-- `isAppInstalled`
-- `startApp`
-- `startUri`
-- `getFocusedPackageAndActivity`
-- `waitForActivityOrNot`
-- `waitForActivity`
-- `waitForNotActivity`
-- `uninstallApk`
-- `installFromDevicePath`
-- `install`
-- `fingerprint` (ApiLevel >=23 | emulator only)
-- `sendSMS` (emulator only)
-- `rotate` (emulator only)
-- `powerAC` (emulator only)
-- `powerCapacity` (emulator only)
-- `powerOFF` (emulator only)
-- `gsmCall` (emulator only)
-- `gsmSignal` (emulator only)
-- `gsmVoice` (emulator only)
-- `root`
-- `unroot`
--- test/unit/adb-commands-specs.js
+++ test/unit/adb-commands-specs.js
@@ -394,18 +394,7 @@ describe('adb commands', withMocks({adb, logcat, teen_process, net}, function (m
       });
     });
     describe('setWifiState', function () {
-      it('should call shell with correct args for real device API 29', async function () {
-        mocks.adb.expects('requireRunningSettingsApp').once();
-        mocks.adb.expects('getApiLevel').once().returns(29);
-        mocks.adb.expects('shell')
-          .once().withExactArgs(['am', 'broadcast', '-a', 'io.appium.settings.wifi',
-            '-n', 'io.appium.settings/.receivers.WiFiConnectionSettingReceiver',
-            '--es', 'setstatus', 'enable'])
-          .returns('');
-        await adb.setWifiState(true);
-      });
-      it('should call shell with correct args for real device API 30', async function () {
-        mocks.adb.expects('getApiLevel').once().returns(30);
+      it('should call shell with correct args for real device', async function () {
         mocks.adb.expects('shell')
           .once().withExactArgs(['cmd', '-w', 'wifi', 'set-wifi-enabled', 'enabled'])
           .returns('');
@@ -437,18 +426,7 @@ describe('adb commands', withMocks({adb, logcat, teen_process, net}, function (m
       });
     });
     describe('setDataState', function () {
-      it('should call shell with correct args for real device API 29', async function () {
-        mocks.adb.expects('getApiLevel').once().returns(29);
-        mocks.adb.expects('requireRunningSettingsApp').once();
-        mocks.adb.expects('shell')
-          .once().withExactArgs(['am', 'broadcast', '-a', 'io.appium.settings.data_connection',
-            '-n', 'io.appium.settings/.receivers.DataConnectionSettingReceiver',
-            '--es', 'setstatus', 'disable'])
-          .returns('');
-        await adb.setDataState(false);
-      });
-      it('should call shell with correct args for real device API 30', async function () {
-        mocks.adb.expects('getApiLevel').once().returns(30);
+      it('should call shell with correct args for real device', async function () {
         mocks.adb.expects('shell')
           .once().withExactArgs(['cmd', 'phone', 'data', 'disable'])
           .returns('');
@@ -463,71 +441,6 @@ describe('adb commands', withMocks({adb, logcat, teen_process, net}, function (m
           })
           .returns('');
         await adb.setDataState(true, true);
-      });
-    });
-    describe('setWifiAndData', function () {
-      it('should call shell with correct args when turning only wifi on for real device', async function () {
-        mocks.adb.expects('getApiLevel').atLeast(1).returns(29);
-        mocks.adb.expects('requireRunningSettingsApp').once();
-        mocks.adb.expects('shell')
-          .once().withExactArgs(['am', 'broadcast', '-a', 'io.appium.settings.wifi',
-            '-n', 'io.appium.settings/.receivers.WiFiConnectionSettingReceiver',
-            '--es', 'setstatus', 'enable'])
-          .returns('');
-        await adb.setWifiAndData({wifi: true});
-      });
-      it('should call shell with correct args when turning only wifi off for emulator', async function () {
-        mocks.adb.expects('getApiLevel')
-          .atLeast(1).returns(25);
-        mocks.adb.expects('shell')
-          .once().withExactArgs(['svc', 'wifi', 'disable'], {
-            privileged: true
-          })
-          .returns('');
-        await adb.setWifiAndData({wifi: false}, true);
-      });
-      it('should call shell with correct args when turning only data on for emulator', async function () {
-        mocks.adb.expects('getApiLevel')
-          .atLeast(1).returns(25);
-        mocks.adb.expects('shell')
-          .once().withExactArgs(['svc', 'data', 'enable'], {
-            privileged: true
-          })
-          .returns('');
-        await adb.setWifiAndData({data: true}, true);
-      });
-      it('should call shell with correct args when turning only data off for real device', async function () {
-        mocks.adb.expects('getApiLevel').atLeast(1).returns(30);
-        mocks.adb.expects('shell')
-          .once().withExactArgs(['cmd', 'phone', 'data', 'disable'])
-          .returns('');
-        await adb.setWifiAndData({data: false});
-      });
-      it('should call shell with correct args when turning both wifi and data on for real device', async function () {
-        mocks.adb.expects('getApiLevel').atLeast(1).returns(29);
-        mocks.adb.expects('requireRunningSettingsApp').atLeast(1);
-        mocks.adb.expects('shell').atLeast(1).returns('');
-        await adb.setWifiAndData({wifi: true, data: true});
-      });
-      it('should call shell with correct args when turning both wifi and data off for emulator', async function () {
-        mocks.adb.expects('getApiLevel').atLeast(1).returns(25);
-        mocks.adb.expects('shell').atLeast(1).returns('');
-        await adb.setWifiAndData({wifi: false, data: false}, true);
-      });
-    });
-    describe('setAnimationState', function () {
-      const adbArgs = [
-        'am', 'broadcast', '-a', 'io.appium.settings.animation',
-        '-n', 'io.appium.settings/.receivers.AnimationSettingReceiver',
-        '--es', 'setstatus'
-      ];
-      it('should call shell with correct args to enable animation', async function () {
-        mocks.adb.expects('shell').once().withExactArgs(adbArgs.concat('enable'));
-        await adb.setAnimationState(true);
-      });
-      it('should call shell with correct args to disable animation', async function () {
-        mocks.adb.expects('shell').once().withExactArgs(adbArgs.concat('disable'));
-        await adb.setAnimationState(false);
       });
     });
     describe('isAnimationOn', function () {
@@ -554,68 +467,6 @@ describe('adb commands', withMocks({adb, logcat, teen_process, net}, function (m
       it('should return true if window_animation_scale setting is NOT equal to zero', async function () {
         mockSetting('0.0', '0.0', '0.5');
         (await adb.isAnimationOn()).should.be.true;
-      });
-    });
-    describe('setDeviceSysLocaleViaSettingApp', function () {
-      it('should call shell with locale settings without script', async function () {
-        const adbArgs = ['am', 'broadcast', '-a', 'io.appium.settings.locale',
-          '-n', 'io.appium.settings/.receivers.LocaleSettingReceiver',
-          '--es', 'lang', 'en', '--es', 'country', 'US'];
-
-        mocks.adb.expects('shell').once().withExactArgs(adbArgs);
-        await adb.setDeviceSysLocaleViaSettingApp('en', 'US');
-      });
-
-      it('should call shell with locale settings with script', async function () {
-        const adbArgs = ['am', 'broadcast', '-a', 'io.appium.settings.locale',
-          '-n', 'io.appium.settings/.receivers.LocaleSettingReceiver',
-          '--es', 'lang', 'zh', '--es', 'country', 'CN', '--es', 'script', 'Hans'];
-        mocks.adb.expects('shell').once().withExactArgs(adbArgs);
-        await adb.setDeviceSysLocaleViaSettingApp('zh', 'CN', 'Hans');
-      });
-    });
-    describe('setGeoLocation', function () {
-      const location = {
-        longitude: '50.5',
-        latitude: '50.1'
-      };
-      it('should call shell with correct args for real device with Oreo preview device', async function () {
-        mocks.adb.expects('getApiLevel')
-          .once().returns(26);
-        mocks.adb.expects('shell')
-          .once().withExactArgs([
-            'am', 'start-foreground-service',
-            '-e', 'longitude', location.longitude,
-            '-e', 'latitude', location.latitude,
-            `io.appium.settings/.LocationService`
-          ])
-          .returns('');
-        await adb.setGeoLocation(location);
-      });
-      it('should call shell with correct args for real device with Nougat preview device', async function () {
-        mocks.adb.expects('getApiLevel')
-          .once().returns(25);
-        mocks.adb.expects('shell')
-          .once().withExactArgs([
-            'am', 'startservice',
-            '-e', 'longitude', location.longitude,
-            '-e', 'latitude', location.latitude,
-            `io.appium.settings/.LocationService`
-          ])
-          .returns('');
-        await adb.setGeoLocation(location);
-      });
-      it('should call adb with correct args for emulator', async function () {
-        mocks.adb.expects('resetTelnetAuthToken')
-          .once().returns(true);
-        mocks.adb.expects('adbExec')
-          .once().withExactArgs(['emu', 'geo', 'fix', location.longitude, location.latitude])
-          .returns('');
-        // A workaround for https://code.google.com/p/android/issues/detail?id=206180
-        mocks.adb.expects('adbExec')
-          .once().withExactArgs(['emu', 'geo', 'fix', location.longitude.replace('.', ','), location.latitude.replace('.', ',')])
-          .returns('');
-        await adb.setGeoLocation(location, true);
       });
     });
     describe('processExists', function () {
@@ -1243,120 +1094,5 @@ describe('adb commands', withMocks({adb, logcat, teen_process, net}, function (m
         'settings delete global hidden_api_policy');
       await adb.setDefaultHiddenApiPolicy();
     });
-  });
-
-  describe('isSettingsAppServiceRunningInForeground', function () {
-    it('should return true if the output includes isForeground=true', async function () {
-      // this case is when 'io.appium.settings/.NLService' was started AND
-      // the settings app is running as a foreground service.
-      // This case could happen when only 'shell cmd notification allow_listener io.appium.settings/.NLService' is
-      // called but the process hasn't been started from io.appium.settings/.ForegroundService,
-      // or the app process was stopped by "Force Stop" via the system settings app.
-      const getActivityServiceOutput = `
-          ACTIVITY MANAGER SERVICES (dumpsys activity services)
-            User 0 active services:
-            * ServiceRecord{f0ad90b u0 io.appium.settings/.NLService}
-              intent={act=android.service.notification.NotificationListenerService cmp=io.appium.settings/.NLService}
-              packageName=io.appium.settings
-              processName=io.appium.settings
-              permission=android.permission.BIND_NOTIFICATION_LISTENER_SERVICE
-              baseDir=/data/app/~~fHuRc6u9ehtAcXvuXy-fiw==/io.appium.settings-wJRwd1HrrbVG5ZINWuHi5Q==/base.apk
-              dataDir=/data/user/0/io.appium.settings
-              app=ProcessRecord{1d61746 18302:io.appium.settings/u0a320}
-              whitelistManager=true
-              allowWhileInUsePermissionInFgs=true
-              startForegroundCount=0
-              recentCallingPackage=android
-              createTime=-6m21s859ms startingBgTimeout=--
-              lastActivity=-6m21s783ms restartTime=-6m21s783ms createdFromFg=true
-              Bindings:
-              * IntentBindRecord{a5d675f CREATE}:
-                intent={act=android.service.notification.NotificationListenerService cmp=io.appium.settings/.NLService}
-                binder=android.os.BinderProxy@1be25ac
-                requested=true received=true hasBound=true doRebind=false
-                * Client AppBindRecord{c78a275 ProcessRecord{3f853b1 1847:system/1000}}
-                  Per-process Connections:
-                    ConnectionRecord{fbf1188 u0 CR FGS !PRCP io.appium.settings/.NLService:@339692b}
-              All Connections:
-                ConnectionRecord{fbf1188 u0 CR FGS !PRCP io.appium.settings/.NLService:@339692b}
-
-            * ServiceRecord{e7a180b u0 io.appium.settings/.ForegroundService}
-              intent={act=start cmp=io.appium.settings/.ForegroundService}
-              packageName=io.appium.settings
-              processName=io.appium.settings
-              permission=android.permission.FOREGROUND_SERVICE
-              baseDir=/data/app/~~fHuRc6u9ehtAcXvuXy-fiw==/io.appium.settings-wJRwd1HrrbVG5ZINWuHi5Q==/base.apk
-              dataDir=/data/user/0/io.appium.settings
-              app=ProcessRecord{1d61746 18302:io.appium.settings/u0a320}
-              allowWhileInUsePermissionInFgs=true
-              startForegroundCount=1
-              recentCallingPackage=io.appium.settings
-              isForeground=true foregroundId=1 foregroundNoti=Notification(channel=main_channel shortcut=null contentView=null vibrate=null sound=null defaults=0x0 flags=0x62 color=0x00000000 vis=PRIVATE)
-              createTime=-5m1s703ms startingBgTimeout=--
-              lastActivity=-5m1s702ms restartTime=-5m1s702ms createdFromFg=true
-              startRequested=true delayedStop=false stopIfKilled=false callStart=true lastStartId=1
-
-            Connection bindings to services:
-            * ConnectionRecord{fbf1188 u0 CR FGS !PRCP io.appium.settings/.NLService:@339692b}
-              binding=AppBindRecord{c78a275 io.appium.settings/.NLService:system}
-              conn=android.app.LoadedApk$ServiceDispatcher$InnerConnection@339692b flags=0x5000101`;
-      mocks.adb.expects('getApiLevel').once().returns(26);
-      mocks.adb.expects('processExists').never();
-      mocks.adb.expects('shell').once().withArgs(['dumpsys', 'activity', 'services', 'io.appium.settings']).returns(getActivityServiceOutput);
-      await adb.isSettingsAppServiceRunningInForeground().should.eventually.true;
-    });
-    it('should return false if the output does not include isForeground=true', async function () {
-      // this case is when 'io.appium.settings/.NLService' was started but
-      // the settings app hasn't been started as a foreground service yet.
-      const getActivityServiceOutput = `
-        ACTIVITY MANAGER SERVICES (dumpsys activity services)
-          User 0 active services:
-          * ServiceRecord{41dde04 u0 io.appium.settings/.NLService}
-            intent={act=android.service.notification.NotificationListenerService cmp=io.appium.settings/.NLService}
-            packageName=io.appium.settings
-            processName=io.appium.settings
-            permission=android.permission.BIND_NOTIFICATION_LISTENER_SERVICE
-            baseDir=/data/app/~~fHuRc6u9ehtAcXvuXy-fiw==/io.appium.settings-wJRwd1HrrbVG5ZINWuHi5Q==/base.apk
-            dataDir=/data/user/0/io.appium.settings
-            app=ProcessRecord{d3b2ed1 18588:io.appium.settings/u0a320}
-            whitelistManager=true
-            allowWhileInUsePermissionInFgs=true
-            startForegroundCount=0
-            recentCallingPackage=android
-            createTime=-2s362ms startingBgTimeout=--
-            lastActivity=-2s283ms restartTime=-2s283ms createdFromFg=true
-            Bindings:
-            * IntentBindRecord{26ce8cd CREATE}:
-              intent={act=android.service.notification.NotificationListenerService cmp=io.appium.settings/.NLService}
-              binder=android.os.BinderProxy@2dbc582
-              requested=true received=true hasBound=true doRebind=false
-              * Client AppBindRecord{24ce493 ProcessRecord{3f853b1 1847:system/1000}}
-                Per-process Connections:
-                  ConnectionRecord{8f3e709 u0 CR FGS !PRCP io.appium.settings/.NLService:@d481010}
-                  ConnectionRecord{bd3f9f8 u0 CR FGS !PRCP io.appium.settings/.NLService:@1c7ed5b}
-            All Connections:
-              ConnectionRecord{bd3f9f8 u0 CR FGS !PRCP io.appium.settings/.NLService:@1c7ed5b}
-              ConnectionRecord{8f3e709 u0 CR FGS !PRCP io.appium.settings/.NLService:@d481010}
-
-          Connection bindings to services:
-          * ConnectionRecord{bd3f9f8 u0 CR FGS !PRCP io.appium.settings/.NLService:@1c7ed5b}
-            binding=AppBindRecord{24ce493 io.appium.settings/.NLService:system}
-            conn=android.app.LoadedApk$ServiceDispatcher$InnerConnection@1c7ed5b flags=0x5000101
-          * ConnectionRecord{8f3e709 u0 CR FGS !PRCP io.appium.settings/.NLService:@d481010}
-            binding=AppBindRecord{24ce493 io.appium.settings/.NLService:system}
-            conn=android.app.LoadedApk$ServiceDispatcher$InnerConnection@d481010 flags=0x5000101`;
-
-      mocks.adb.expects('getApiLevel').once().returns(26);
-      mocks.adb.expects('processExists').never();
-      mocks.adb.expects('shell').once().withArgs(['dumpsys', 'activity', 'services', 'io.appium.settings']).returns(getActivityServiceOutput);
-      await adb.isSettingsAppServiceRunningInForeground().should.eventually.false;
-    });
-    it('should rely on processExists for api level 25 and lower', async function () {
-      mocks.adb.expects('getApiLevel').once().returns(25);
-      mocks.adb.expects('processExists').once().returns(1000);
-      mocks.adb.expects('shell').never().withArgs(['dumpsys', 'activity', 'services', 'io.appium.settings']);
-      await adb.isSettingsAppServiceRunningInForeground().should.eventually.eql(1000);
-    });
-
   });
 }));
