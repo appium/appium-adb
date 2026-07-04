6bb4e18c1242f403839fab24a09a4b6d966566a6
Mykola Mokhnach <mokhnach@gmail.com>
2026-07-04 22:58:55 +0200

    moar

--- test/functional/lock-mgmt-e2e.spec.ts
+++ test/functional/lock-mgmt-e2e.spec.ts
@@ -1,7 +1,7 @@
 import {ADB} from '../../lib/adb';
 import chai, {expect} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import {describe, it, before, afterEach, type TestContext} from 'node:test';
+import {describe, it, before, type TestContext} from 'node:test';
 
 chai.use(chaiAsPromised);
 
@@ -24,25 +24,25 @@ describe('Lock Management', function () {
   describe('Lock and unlock life cycle', function () {
     const password = '1234';
 
-    afterEach(async function () {
-      await adb.clearLockCredential(password);
-    });
-
     it('device lock and unlock scenario should work', async function (ctx: TestContext) {
       // We don't want to lock the device for all other tests if this test fails
       if (process.env.CI) {
         return ctx.skip();
       }
 
-      await adb.setLockCredential('password', password);
-      await adb.keyevent(26);
-      await expect(adb.isLockEnabled()).to.eventually.be.true;
-      await expect(adb.isScreenLocked()).to.eventually.be.true;
-      await adb.clearLockCredential(password);
-      await adb.cycleWakeUp();
-      await adb.dismissKeyguard();
-      await expect(adb.isLockEnabled()).to.eventually.be.false;
-      await expect(adb.isScreenLocked()).to.eventually.be.false;
+      try {
+        await adb.setLockCredential('password', password);
+        await adb.keyevent(26);
+        await expect(adb.isLockEnabled()).to.eventually.be.true;
+        await expect(adb.isScreenLocked()).to.eventually.be.true;
+        await adb.clearLockCredential(password);
+        await adb.cycleWakeUp();
+        await adb.dismissKeyguard();
+        await expect(adb.isLockEnabled()).to.eventually.be.false;
+        await expect(adb.isScreenLocked()).to.eventually.be.false;
+      } finally {
+        await adb.clearLockCredential(password);
+      }
     });
   });
 });
